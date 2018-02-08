aa9dab468b77b7e03d2a3b68cdc37189750539ba
unrealwork <unrealwork@gmail.com>
2018-02-09 00:05:55 +0500

    chapter 4 finished

--- src/org/springframework/chapter4/MyApp.java
+++ src/org/springframework/chapter4/MyApp.java
@@ -1,11 +1,18 @@
 package org.springframework.chapter4;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MyApp {
 
   public static void main(String[] args) {
-    //create object
-    Coach coach = new BaseballCoach();
-    //use object
+    // load the spring configuration file
+    ClassPathXmlApplicationContext context =
+        new ClassPathXmlApplicationContext("applicationContext.xml");
+    //retrieve bean
+    Coach coach = context.getBean("myCoach", Coach.class);
+    //call methods of the bean
     System.out.println(coach.getDailyWorkout());
+    // close the context
+    context.close();
   }
 }
---
+++ src/org/springframework/chapter4/SwimmingCoach.java
@@ -0,0 +1,9 @@
+package org.springframework.chapter4;
+
+public class SwimmingCoach implements Coach {
+
+  @Override
+  public String getDailyWorkout() {
+    return "Swim 2km";
+  }
+}
